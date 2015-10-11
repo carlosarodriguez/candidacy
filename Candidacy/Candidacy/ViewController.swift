96dcce686c9f5d793a77073e6264fab0c0055f2d
Kevin Avila <kevin.avila@utexas.edu>
2015-10-11 16:01:38 -0500

    created xcode proj

---
+++ Candidacy/Candidacy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Candidacy
+//
+//  Created by Kevin Avila on 10/11/15.
+//  Copyright © 2015 CS378. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
