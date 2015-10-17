f7fd681a3d067f890f14d8a5fae2317ea6fc5317
Kevin Avila <kevin.avila@utexas.edu>
2015-10-17 16:28:55 -0500

    added candidates view controller. need to sync VC's with menu

---
+++ Candidacy/Candidacy/CandidatesViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CandidatesViewController.swift
+//  Candidacy
+//
+//  Created by Kevin Avila on 10/17/15.
+//  Copyright © 2015 CS378. All rights reserved.
+//
+
+import UIKit
+import REFrostedViewController
+
+
+class CandidatesViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func showMenu(sender: AnyObject) {
+        self.view.endEditing(true)
+        self.frostedViewController.view.endEditing(true)
+        self.frostedViewController.presentMenuViewController()
+    }
+    
+    
+}
+
--- Candidacy/Candidacy/MenuViewController.swift
+++ Candidacy/Candidacy/MenuViewController.swift
@@ -11,4 +11,37 @@ import UIKit
 
 class MenuViewController: UITableViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
+    }
+    
+//    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        
+//        var cellIdentifier: String = "menuCell"
+//        var cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier)!
+//        cell = UITableViewCell(style: UITableViewCellStyleDefault, reuseIdentifier: cellIdentifier)
+//        if indexPath.section == 0 {
+//            var titles: [AnyObject] = ["Home", "Profile", "Chats"]
+//            cell.textLabel.text = titles[indexPath.row]
+//        }
+//        else {
+//            var titles: [AnyObject] = ["John Appleseed", "John Doe", "Test User"]
+//            cell.textLabel.text = titles[indexPath.row]
+//        }
+//        return cell
+//    }
+    
 }
