c8f94044ce7b66d72b6a2facefe240c898e25488
Sam Wang <wang79922@gmail.com>
2015-12-03 06:11:42 -0600

    updated show results

---
+++ Candidacy/Candidacy/PollViewStats.swift
@@ -0,0 +1,22 @@
+//
+//  PollViewStats.swift
+//  Candidacy
+//
+//  Created by Sam Wang on 12/3/15.
+//  Copyright © 2015 CS378. All rights reserved.
+//
+
+import Foundation
+import Parse
+import UIKit
+
+class PollViewStats: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+}
